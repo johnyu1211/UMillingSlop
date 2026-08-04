0b016cb1bd6d39c43152b517ce53a0287b415124
YEEEEEEEEEEE <47817667+johnyu1211@users.noreply.github.com>
2026-08-04 21:33:42 +0900

    Set humanoid bullet color to mint #52CBBC

--- game.js
+++ game.js
@@ -4143,7 +4143,7 @@ function updateEnemies(deltaTime) {
                                     velocityX: Math.cos(curAngle) * 6.5,
                                     velocityY: Math.sin(curAngle) * 6.5,
                                     size: 7,
-                                    color: (enemy.bodyType === 'assault_humanoid') ? '#FF1133' : (enemy.bodyType === 'machinegun_humanoid') ? '#52CBBC' : bColor,
+                                    color: (enemy.bodyType === 'assault_humanoid') ? '#52CBBC' : bColor,
                                     tier: enemy.tier || 1
                                 });
                             }
